d9111dfafd7ca3b2674790f1d3b219a578e7aadc
celinechole <github@celinechole.com>
2019-02-26 15:58:48 -0500

    Implement products model

---
+++ server/db/models/products.js
@@ -0,0 +1,29 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const products = db.define('products', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  country: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  type: {
+    type: Sequelize.ARRAY(Sequelize.STRING),
+    allowNull: false
+  },
+  Scoville: {
+    type: Sequelize.BIGINT,
+    allowNull: false,
+    validate: {
+      min: 0,
+      max: 3200000
+    }
+  }
+})
