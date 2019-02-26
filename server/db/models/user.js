fde4f424325ae594c9b166a7f53f69aab64bf77b
Wendy Sung <wendalssung@gmail.com>
2019-02-26 16:33:22 -0500

    create user models

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -3,13 +3,28 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const User = db.define('user', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
   email: {
     type: Sequelize.STRING,
     unique: true,
     allowNull: false
   },
+  birthday: {
+    type: Sequelize.DATE
+  },
+  isAdmin: {
+    type: Sequelize.BOOLEAN,
+    defaultValue: false
+  },
   password: {
     type: Sequelize.STRING,
+    allowNull: false,
     // Making `.password` act like a func hides it when serializing to JSON.
     // This is a hack to get around Sequelize's lack of a "private" option.
     get() {
