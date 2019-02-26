d6d367ea5ee7ddec229ad5d2ae855a4a46078f35
Bianca N <bianca.neptune@gmail.com>
2019-02-26 17:35:25 -0500

    all and single product routes

---
+++ server/api/products.js
@@ -0,0 +1,21 @@
+const router = require('express').Router()
+const {Products} = require('../db/models')
+module.exports = router
+
+router.get('/', async (req, res, next) => {
+  try {
+    const products = await Products.findAll()
+    res.json(products)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.get('/:productId', async (req, res, next) => {
+  try {
+    const singleProduct = await Products.findById(Number(req.params.productId))
+    res.json(singleProduct)
+  } catch (err) {
+    next(err)
+  }
+})
