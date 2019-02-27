455d8ae4ed7682a7b19df54ccc1ab0352c647766
JustineLee103 <45375814+JustineLee103@users.noreply.github.com>
2019-02-26 19:51:18 -0500

    Feat: fix user spec (#7)

--- server/db/models/user.spec.js
+++ server/db/models/user.spec.js
@@ -15,13 +15,22 @@ describe('User model', () => {
 
       beforeEach(async () => {
         cody = await User.create({
-          email: 'cody@puppybook.com',
-          password: 'bones'
+          name: 'Bob',
+          email: 'bobby@email.com',
+          password: '123',
+          shippingAddress: '23 Spicey Court',
+          shippingCity: 'Orlando',
+          shippingState: 'Florida',
+          shippingZip: '11211',
+          billingAddress: '1 Hot Sauce Way',
+          billingState: 'New York',
+          billingCity: 'New York',
+          billingZip: '32789'
         })
       })
 
       it('returns true if the password is correct', () => {
-        expect(cody.correctPassword('bones')).to.be.equal(true)
+        expect(cody.correctPassword('123')).to.be.equal(true)
       })
 
       it('returns false if the password is incorrect', () => {
