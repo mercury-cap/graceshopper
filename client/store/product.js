d2c9a68883efca000f7b9d09cd6eb1c1e9d21ffa
celinechole <github@celinechole.com>
2019-02-27 11:32:12 -0500

    implement product reducer for all products and single product

--- client/store/product.js
+++ client/store/product.js
@@ -1,1 +1,45 @@
 import axios from 'axios'
+
+const GET_ALL_PRODUCTS = 'GET_ALL_PRODUCTS'
+const GET_SINGLE_PRODUCT = 'GET_SINGLE_PRODUCT'
+
+const getAllProducts = products => ({
+  type: GET_ALL_PRODUCTS,
+  products
+})
+
+const getSingleProduct = singleProduct => ({
+  type: GET_SINGLE_PRODUCT,
+  singleProduct
+})
+
+export const getAllProducts = () => {
+  return async dispatch => {
+    const response = await axios.get('/api/products')
+    const products = response.data
+    const action = getAllProducts(products)
+    dispatch(action)
+  }
+}
+
+export const getSingleProduct = id => {
+  return async dispatch => {
+    const response = await axios.get(`/api/products/${id}`)
+    const singleProduct = response.data
+    const action = getSingleProduct(singleProduct)
+    dispatch(action)
+  }
+}
+
+const initialState = []
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case GET_ALL_PRODUCTS:
+      return action.products
+    case GET_SINGLE_PRODUCT:
+      return action.singleProduct
+    default:
+      return state
+  }
+}
