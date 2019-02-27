2393565cca7892ab5005835639808dcfeeee9a41
CelineChole <github@celinechole.com>
2019-02-27 18:23:04 -0500

    Refine product view (#31)
    * feat: single product quantity
    
    * feat: work on updating cart
    
    * test

--- client/store/product.js
+++ client/store/product.js
@@ -2,6 +2,8 @@ import axios from 'axios'
 
 const GET_ALL_PRODUCTS = 'GET_ALL_PRODUCTS'
 const GET_SINGLE_PRODUCT = 'GET_SINGLE_PRODUCT'
+const SET_SUBTOTAL = 'SET_SUBTOTAL'
+const SET_CART = 'SET_CART'
 
 export const gotAllProducts = products => ({
   type: GET_ALL_PRODUCTS,
@@ -11,6 +13,16 @@ export const gotAllProducts = products => ({
 export const getProduct = product => ({
   type: GET_SINGLE_PRODUCT,
   product
+})
+
+// export const setSubtotal = (subtotal) => ({
+//   type: SET_SUBTOTAL,
+//   subtotal
+// })
+
+export const setCart = cartItems => ({
+  type: SET_CART,
+  cartItems
 })
 
 export const getAllProducts = () => {
@@ -33,7 +45,7 @@ export const getSingleProduct = id => {
 const initialState = {
   products: [],
   singleProduct: {},
-  subtotal: 0
+  cart: []
 }
 
 export default function(state = initialState, action) {
@@ -42,7 +54,12 @@ export default function(state = initialState, action) {
       return {...state, products: action.products}
     case GET_SINGLE_PRODUCT:
       return {...state, singleProduct: action.product}
+    // case SET_SUBTOTAL:
+    //   return {...state, subtotal: action.subtotal}
+    case SET_CART:
+      return {...state, cart: [...state.cart, ...action.cartItems]}
     default:
       return state
   }
 }
+// add a ACTION to clear the cart upon checkout
