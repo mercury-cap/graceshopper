76c26df3e755a347b8b2c001d63986e0f2e5b306
Wendy Sung <wendalssung@gmail.com>
2019-03-01 16:51:52 -0500

    add order summary incl total

--- client/components/Checkout.js
+++ client/components/Checkout.js
@@ -1,11 +1,24 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
+import {getCartItems} from '../store/product'
 
 class Checkout extends Component {
   constructor(props) {
     super(props)
-    this.state = {}
+    this.state = {
+      items: [],
+      subtotal: 0
+    }
     this.handleClick = this.handleClick.bind(this)
+  }
+  componentDidMount = async () => {
+    await this.props.getCartItems()
+    this.setState({
+      items: this.props.items,
+      subtotal: (
+        this.props.items.reduce((sum, item) => sum + item.price, 0) / 100
+      ).toFixed(2)
+    })
   }
 
   handleClick = () => {
@@ -13,11 +26,21 @@ class Checkout extends Component {
   }
 
   render() {
-    console.log('Items:', this.props.items)
     return (
       <div>
         <h1>ORDER SUMMARY</h1>
-        {this.props.items.map(item => <p key={item.name}>{item.name}</p>)}
+        <div id="checkout-items">
+          {this.state.items.map(item => (
+            <div id="checkout-single-item" key={item.id}>
+              <p>{item.name}</p>
+              <p>${(item.price / 100).toFixed(2)}</p>
+            </div>
+          ))}
+          <h3>Total before tax:</h3>
+          <p>${this.state.subtotal}</p>
+          <h3>Tax:</h3>
+          <h1>Order Total</h1>
+        </div>
 
         <button type="submit" onClick={this.handleClick}>
           Place Your Order
@@ -27,14 +50,12 @@ class Checkout extends Component {
   }
 }
 
-const mapStateToProps = state => {
-  return {
-    items: state.product.cart
-  }
-}
+const mapStateToProps = state => ({
+  items: state.product.cart
+})
 
-const mapDispatchToProps = dispatch => {
-  return {}
-}
+const mapDispatchToProps = dispatch => ({
+  getCartItems: () => dispatch(getCartItems())
+})
 
 export default connect(mapStateToProps, mapDispatchToProps)(Checkout)
