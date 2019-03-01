4cb16285116a02f6e91749e6f973f12222b410bd
Justine Lee <justinelee103@gmail.com>
2019-03-01 18:07:01 -0500

    Feat: create delete route

--- client/store/product.js
+++ client/store/product.js
@@ -4,6 +4,7 @@ const GET_ALL_PRODUCTS = 'GET_ALL_PRODUCTS'
 const GET_SINGLE_PRODUCT = 'GET_SINGLE_PRODUCT'
 const UPDATE_CART = 'UPDATE_CART'
 const GOT_CART_ITEMS = 'GOT_CART_ITEMS'
+const REMOVE_ITEM = 'REMOVE_ITEM'
 
 export const gotAllProducts = products => ({
   type: GET_ALL_PRODUCTS,
@@ -22,6 +23,11 @@ const updateCart = item => ({
 
 const gotCartItems = items => ({
   type: GOT_CART_ITEMS,
+  items
+})
+
+const removeItem = items => ({
+  type: REMOVE_ITEM,
   items
 })
 
@@ -50,6 +56,14 @@ export const updateCartInServer = item => async dispatch => {
 export const getCartItems = () => {
   return async dispatch => {
     const {data: items} = await axios.get('/api/users/cart')
+    dispatch(gotCartItems(items))
+  }
+}
+
+export const removeItem = () => {
+  return async dispatch => {
+    const response = await axios.destroy('/api/users/cart')
+    const singleProduct = response.data
     dispatch(gotCartItems(items))
   }
 }
