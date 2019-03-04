894d292f90a7236b0965812a9e26744ed77b6ed7
celinechole <github@celinechole.com>
2019-03-04 14:24:15 -0500

    style: minimum css to look consistent with other pages

--- client/components/OrderSummary.js
+++ client/components/OrderSummary.js
@@ -2,10 +2,14 @@ import React from 'react'
 
 const OrderSummary = props => {
   return (
-    <div>
-      <h1>Thank you for your order!</h1>
-      <p>Here is your order # for your reference:</p>
-      <p>Order #{props.match.params.cartId}</p>
+    <div className="container center-align">
+      <div className="row">
+        <h2 className="orange-text">Thank you for your order!</h2>
+        <div className="row">
+          <p>Here is your order # for your reference:</p>
+          <p>Order #{props.match.params.cartId}</p>
+        </div>
+      </div>
     </div>
   )
 }
