054756467aad6fc25ffe368524d8092f5ab4dbe7
Wendy Sung <wendalssung@gmail.com>
2019-03-04 22:23:17 -0500

    feat (all products view): change link to card level

--- client/components/AllProducts.js
+++ client/components/AllProducts.js
@@ -43,18 +43,18 @@ class AllProducts extends Component {
 
     const productCard = productList.map(product => {
       return (
-        <div className="col s12 m6 l3" key={product.id}>
-          <div className="card small">
-            <div className="card-image">
-              <img src={product.imageUrl} />
-            </div>
-            <div className="card-content">
-              <NavLink to={`/products/${product.id}`}>
+        <NavLink to={`/products/${product.id}`} key={product.id}>
+          <div className="col s12 m6 l3">
+            <div className="card small">
+              <div className="card-image">
+                <img src={product.imageUrl} />
+              </div>
+              <div className="card-content">
                 <div className="orange-text">{product.name}</div>
-              </NavLink>
+              </div>
             </div>
           </div>
-        </div>
+        </NavLink>
       )
     })
 
